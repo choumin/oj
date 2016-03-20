d282c3faf310754ac1cfa5a2744aa947c780f4d5
choumin <choumin@choumins-MacBook-Air.local>
2016-03-20 21:52:52 +0800

    modify float_sum.c

--- float_sum/float_sum.c
+++ float_sum/float_sum.c
@@ -1,55 +1,141 @@
 #include <stdio.h>
+#include <string.h>
 
-#define MAX_COUNT	204
+#define MAX_COUNT	104
 
-char f1[MAX_COUNT];
-char f2[MAX_COUNT];
-char ret[MAX_COUNT];
+char f1[2][MAX_COUNT];
+char f2[2][MAX_COUNT];
+char ret_int[MAX_COUNT];
+char ret_dec[MAX_COUNT];
 
+int add_str(char *str1, int end1, char *str2, int end2, int c, char *ret_str, int mode)
+{
+	int max = end1 > end2 ? end1 : end2;
+	int i = 0;
+	int sum = 0;
+	int j = max;
+	int delt = end1 - end2 > 0 ? end1 - end2 : end2 - end1;
+	char *p = NULL;
+	int k = 0;
+
+	if (mode == 0) {
+		if (end1 < end2) {
+			p = str2;
+			k = end2;
+		} else if (end1 > end2) {
+			p = str1;
+			k = end1;
+		}
+		for (i = 0; i < delt; i++) {
+			ret_str[j--] = p[k--];
+		}
+
+		max -= delt;
+		for (i = max; i > 0; i--) {
+			sum = c;
+			sum += str1[i] - '0' + str2[i] - '0';	
+			ret_str[j--] = sum % 10 + '0';
+			c = sum /10;
+		} 
+	} else {
+		if (end1 < end2) {
+			for (i = end2; i > 0; i--) {
+				sum = c;
+				if (end1 > 0) {
+					sum += str1[end1--] - '0' + str2[end2--] - '0';	
+				} else {
+					sum += str2[end2--] - '0';
+				}
+				
+				ret_str[i] = sum % 10 + '0';
+				c = sum / 10;
+			}
+		} else {
+			for (i = end1; i > 0; i--) {
+				sum = c;
+				if (end2 > 0) {
+					sum += str1[end1--] - '0' + str2[end2--] - '0';	
+				} else {
+					sum += str1[end1--] - '0';
+				}
+				
+				ret_str[i] = sum % 10 + '0';
+				c = sum / 10;
+			}
+		}
+	}
+
+	return c;
+}
 int deal_one_line(void)
 {
 	char ch = 0;
 	int ret = 0;
 	int i = 0;
 	int j = 0;
-	int ilen1 = 0;
-	int dlen1 = 0;
-	int ilen2 = 0;
-	int dlen2 = 0;
-	int c = 0;
-	int max_len = 0;
+	int c1 = 0;
+	int c2 = 0;
+	char *p = NULL;
+	int max = 0;
+
+	for (i = 0; i < MAX_COUNT; i++) {
+		ret_int[i] = 0;
+		ret_dec[i] = 0;
+	}
 
 	while (scanf("%c", &ch) != EOF && ch == '\n') {
 		;
 	}
 	j = 0;
-	f1[++j] = ch;
-	while (scanf("%c", &ch) != EOF && ch != '\n')) {
+	p = f1[0];
+	p[++j] = ch;
+	while (scanf("%c", &ch) != EOF && ch != '\n') {
 		if (ch == '.') {
-			idlen1 = j;
+			p[0] = j;
+			p = f1[1];
+			j = 0; 
+		} else {
+			p[++j] = ch;	
 		}
-		f1[++j] = ch;
 	}
-	f1[0] = j;
-	dlen1 = j - ilen1 - 1;
+	p[0] = j;
 
 	j = 0;
+	p = f2[0];
 	scanf("%c", &ch);
-	f2[++j] = ch;
-	while (scanf("%c", &ch != EOF && ch != '\n')) {
+	p[++j] = ch;
+	while (scanf("%c", &ch) != EOF && ch != '\n') {
 		if (ch == '.') {
-			idlen2 = j;
+			p[0] = j;
+			p = f2[1];
+			j = 0;
+		} else {
+			p[++j] = ch;
 		}
-		f2[++j] = ch;
 	}
-	f2[0] = j;
-	dlen2 = j - ilen2 - 1;
+	p[0] = j;
 
-	if (dlen1 > dlen1) {
-		if ()
+	c1 = add_str(f1[1], f1[1][0], f2[1], f2[1][0], 0, ret_dec, 0);
+	c2 = add_str(f1[0], f1[0][0], f2[0], f2[0][0], c1, ret_int, 1);
+	ret_int[0] = c2 + '0';
+
+	p = ret_int;
+	if (!c2) {
+		p++;
+	} 
+	max = f1[1][0] > f2[1][0] ? f1[1][0] : f2[1][0];
+	while (ret_dec[max] == '0') {
+		ret_dec[max--] = 0;
 	}
-	max_len = f2[0] > f1[0] ? f2[0] : f1[0]; 
-	for () 
+	printf("%s", p);
+	if (strlen(ret_dec + 1)) {
+		printf(".%s\n", ret_dec + 1);	
+	} else {
+		printf("\n");
+	}
+	
+	
+	return 0;
 }
 int main(void)
 {
